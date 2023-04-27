99de9b77096e7e5e0f866b73abd2205eff35560d
nedu-ramzi <ramseychinedu@gmail.com>
2023-04-27 05:03:57 +0000

    web server

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,46 @@
+# install nginx with puppet
+
+class nginx {
+   package { 'nginx':
+     ensure => installed,
+  }
+
+  service { 'nginx':
+    ensure => running,
+    enable => true,
+    }
+
+   file { '/var/www/html/index.html':
+     ensure => present,
+     content => "Hello World!\n",
+   }
+
+   file { '/etc/nginx/sites-available/default':
+     ensure => present,
+     content => "
+       server {
+         listen 80 default_server;
+	 listen [::]:80 default_server;
+	 server_name _;
+	 root /var/www/html;
+	 index index.html;
+	 location /redirect_me {
+	   return 301 /;
+	   }
+	  }
+	 "},
+	}
+
+   file { '/etc/nginx/sites-enabled/default':
+     ensure => 'link',
+     target => '/etc/nginx/sites-available/default',
+   }
+
+   exec { 'nginx_reload':
+     command     => '/usr/sbin/service nginx reload',
+     refreshonly => true,
+     }
+   }
+
+   include nginx
+
