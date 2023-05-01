8b44afba610299d4c33799c120574b3a260d41cc
nedu-ramzi <ramseychinedu@gmail.com>
2023-05-01 15:16:07 +0000

    Load Balancer

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,27 @@
+# installs an nginx server and creates a custom http header response
+
+# install nginx
+exec { 'update':
+  provider => shell,
+  command  => 'sudo apt-get -y update',
+  before   => Exec['install Nginx'],
+}
+
+exec { 'install Nginx':
+  provider => shell,
+  command  => 'sudo apt-get -y install nginx',
+  before   => Exec['add_header'],
+}
+
+# add custom header
+exec { 'add_header':
+  provider    => shell,
+  command     => 'sudo sed -i "s/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;\n\tadd_header X-Served-By \"$hostname\";/" /etc/nginx/nginx.conf'
+  before      => Exec['restart Nginx'],
+}
+
+# restart nginx services when the header changes have been made
+exec { 'restart Nginx':
+  provider => shell,
+  command  => 'sudo service nginx restart',
+}
