d2b250680d9be37998b9f2236afa405b96b26c1d
nedu-ramzi <ramseychinedu@gmail.com>
2023-05-25 17:09:55 +0000

    a Python script that, using this REST API, for a given employee ID, returns information about his/her TODO list progress.

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,34 +1,31 @@
 #!/usr/bin/python3
-"""
-    Script that uses a restful api for a given employee ID,
-    returns information about his/her TODO list progress.
-"""
+"""Accessing a REST API for todo lists of employees"""
+
 import requests
 import sys
 
 
-def get_employee_todo_progress(employee_id):
-	"""Makes a GET request to the restful API endpoint"""
-	res = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'
-			   .format(employee_id))
-	todos = res.json()
-
-	# filter completed task
-	completed_tasks = [todo for todo in todos if todo['completed']]
-
-	# get employee name
-	user_respons = requests.get('https://jsonplaceholder.typicode.com/users/{}'
-				    .format(employee_id))
-	user = user_respons.json()
-	employee_name = user['name']
-
-	# print employee todo list
-	print('Employee {} is done with tasks({}/{}):'
-	      .format(employee_name, len(completed_tasks), len(todos)))
-	for task in completed_tasks:
-	    print('\t {}'.format(task['title']))
-
-
 if __name__ == '__main__':
-    employee_id = int(sys.argv[1])
-    get_employee_todo_progress(employee_id)
+    employeeId = sys.argv[1]
+    baseUrl = "https://jsonplaceholder.typicode.com/users"
+    url = baseUrl + "/" + employeeId
+
+    response = requests.get(url)
+    employeeName = response.json().get('name')
+
+    todoUrl = url + "/todos"
+    response = requests.get(todoUrl)
+    tasks = response.json()
+    done = 0
+    done_tasks = []
+
+    for task in tasks:
+        if task.get('completed'):
+            done_tasks.append(task)
+            done += 1
+
+    print("Employee {} is done with tasks({}/{}):"
+          .format(employeeName, done, len(tasks)))
+
+    for task in done_tasks:
+        print("\t {}".format(task.get('title')))
